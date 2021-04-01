fc2c3d21a3ffb253b461ebb31811425f45a79b49
sricharankrishnan <sricharan.krishnan@gmail.com>
2021-04-01 19:10:43 +0530

    Feature Update for 01042021_1910 - Init   1. created required directory structure for the app   2. created common css style requirements with sass   3. established font sizes for the app   4. removed unwanted files
    - FoodOrderingApplication
      1. initialized central app file
      2. imported required css from common.scss and variables.scss
      3. demo welcome page built for team

---
+++ src/FoodOrderingApplication.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FoodOrderingApplication extends Component {
+  render() {
+    return (
+      <div className="demo">
+        <h1>Food Ordering Application</h1>
+        <p>Welcome Screen</p>
+      </div>
+    );
+  }
+}
+export default FoodOrderingApplication;
