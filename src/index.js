e588dba38241a9c106f43a295b2a090d3201724a
devatearth <64774809+devatearth@users.noreply.github.com>
2021-04-03 13:47:05 +0530

    Added controller

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 /* react imports */
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Controller from './screens/Controller';
 /* project imports */
 import './index.scss';
-import FoodOrderingApplication from "./FoodOrderingApplication.js";
+//import FoodOrderingApplication from "./FoodOrderingApplication.js";
 
 
 ReactDOM.render(
   <React.Fragment>
-    <FoodOrderingApplication/>
+    <Controller/>
   </React.Fragment>,
   document.getElementById('root')
 );
