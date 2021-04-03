7033f5bbd4268fc90574ebc4ccff4ddbf5dd781e
devatearth <64774809+devatearth@users.noreply.github.com>
2021-04-03 13:47:05 +0530

    Added controller

---
+++ src/screens/checkout/Checkout.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './Checkout.css';
+{console.log("checkout page hitted")}
+class Checkout extends Component {
+    
+    render() {
+        return (
+            <div>Checkot page</div>
+        );
+    }
+}
+export default (Checkout);
